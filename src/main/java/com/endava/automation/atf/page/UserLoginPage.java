3c0d1b727fc7008e484f38bca5b077fe08c433ce
Vpasat <fanykular@gmail.com>
2023-10-06 17:46:56 +0300

    Adding multiple functions

--- src/main/java/com/endava/automation/atf/page/UserLoginPage.java
+++ src/main/java/com/endava/automation/atf/page/UserLoginPage.java
@@ -2,69 +2,56 @@ package com.endava.automation.atf.page;
 
 import com.endava.automation.atf.constant.Users;
 import lombok.Getter;
-import lombok.extern.log4j.Log4j;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import static org.junit.Assert.assertEquals;
-
-@Log4j
 @Getter
 public class UserLoginPage extends AbstractPage {
 
-    @FindBy(id = "user-name")
-    private WebElement userNameField;
-
-    @FindBy(id = "password")
-    private WebElement passwordField;
-
-    @FindBy(id = "login-button")
-    private WebElement loginButton;
-
-    @FindBy(xpath = "//*[@id=\"header_container\"]/div[2]/span")
-    private WebElement assertThatImLoggedIn;
-
-    @FindBy(id = "react-burger-menu-btn")
+    @FindBy(css = "[href*='account/account'].dropdown-toggle")
     private WebElement myAccountButton;
 
+    @FindBy(css = "[href*='account/login']")
+    private WebElement loginButton;
 
-    @FindBy(xpath = "//span[@class='title' and text()='Products']\n")
-    private WebElement assertHomePage;
+    @FindBy(id = "input-email")
+    private WebElement emailField;
 
+    @FindBy(id = "input-password")
+    private WebElement passwordField;
+
+    @FindBy(xpath = "//*[@type='submit']")
+    private WebElement submitButton;
 
     public UserLoginPage(WebDriver driver) {
         super(driver);
     }
 
-    @FindBy(id = "logout_sidebar_link")
+    @FindBy(css = "[href*='account/logout']")
     private WebElement logoutButton;
-
+    @FindBy(css = ".pull-right [href*='common/home']")
+    private WebElement continueButton;
 
     public void userLogin(Users user) throws InterruptedException {
+
+        wait.until(ExpectedConditions.elementToBeClickable(myAccountButton));
+        myAccountButton.click();
         wait.until(ExpectedConditions.elementToBeClickable(loginButton));
-        userNameField.sendKeys(user.getUsername());
-        passwordField.sendKeys(user.getPassword());
         loginButton.click();
-        try {
-            // Wait for the element to be visible before asserting its text
-            wait.until(ExpectedConditions.visibilityOf(assertHomePage));
-            assertEquals("Products", assertHomePage.getText());
-            log.info("Login successful");
-        } catch (TimeoutException | AssertionError e) {
-            // Handle the timeout or assertion error
-            e.printStackTrace();
-            log.info("Login failed");
-        }
+        wait.until(ExpectedConditions.elementToBeClickable(passwordField));
+        emailField.sendKeys(user.getUsername());
+        passwordField.sendKeys(user.getPassword());
+        submitButton.click();
     }
 
-    public void userLogout(Users user) throws InterruptedException {
+    public void userLogout(Users user) {
         wait.until(ExpectedConditions.elementToBeClickable(myAccountButton));
         myAccountButton.click();
         wait.until(ExpectedConditions.elementToBeClickable(logoutButton));
-        Thread.sleep(1000);
         logoutButton.click();
+        wait.until(ExpectedConditions.elementToBeClickable(continueButton));
+        continueButton.click();
     }
 }
